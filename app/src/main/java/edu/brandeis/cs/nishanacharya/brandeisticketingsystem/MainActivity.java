cebba16df0f48544c40963056e51ffdab07f037a
Nishan Acharya <nishan.acharya@gmail.com>
2017-12-02 13:15:21 -0500

    added firebase authentication for creating users, login not complete yet

--- app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/MainActivity.java
+++ app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.brandeis.cs.nishanacharya.brandeisticketingsystem;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,17 +8,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editEmailAddress;
     private EditText editPassword;
     private Button buttonSignUp;
+    private FirebaseAuth firebaseAuth;
     private static final String UNIVERSITY_EMAIL = "@brandeis.edu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
         loginView();
     }
 
@@ -38,7 +46,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void registerUser() {
         String emailAddress = editEmailAddress.getText().toString().trim();
         String password = editPassword.getText().toString().trim();
-        validation(emailAddress, password);
+        if(validation(emailAddress, password)){
+            firebaseAuth.createUserWithEmailAndPassword(emailAddress, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                loginUser();
+                            } else {
+                                //Singup Failure Message for now
+                                Toast.makeText(MainActivity.this, "SignUp Failed", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        }
+    }
+
+    private void loginUser() {
+        //user created, login happens here
     }
 
     private boolean validation(String emailAddress, String password) {
