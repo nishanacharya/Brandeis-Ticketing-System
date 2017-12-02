383b8f8c73e63fb2e25a6c038374fe20754f9c57
Rebecca Shi <rebeccashi@Rebeccas-MacBook-Air-3.local>
2017-12-02 16:16:36 -0500

    QRcode reader, contains camera permissions error

--- app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/MainActivity.java
+++ app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/MainActivity.java
@@ -14,15 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button button_generate = (Button) findViewById(R.id.main_generate);
+        final Button button_read = (Button) findViewById(R.id.main_read);
 
         button_generate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 generate_QR(v);
             }
-        });}
+        });
+        button_read.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                read_QR(v);
+            }
+        });
+    }
+
     public void generate_QR(View view) {
         Intent intent_generate = new Intent(this, QRGenerator.class);
         startActivity(intent_generate);
+    }
+
+    public void read_QR(View view) {
+        Intent intent_read = new Intent(this, QRReader.class);
+        startActivity(intent_read);
     }
 
     public String test(String test){ return test; }
---
+++ app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/QRReader.java
@@ -0,0 +1,56 @@
+package edu.brandeis.cs.nishanacharya.brandeisticketingsystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.Result;
+
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+/**
+ * Created by rebeccashi on 12/2/17.
+ */
+
+public class QRReader extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView ScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_reader);
+
+        Button button = (Button) findViewById(R.id.read);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ScannerView = new ZXingScannerView(QRReader.this);
+                setContentView(ScannerView);
+                ScannerView.setResultHandler(QRReader.this);
+                ScannerView.startCamera();
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        ScannerView.stopCamera();
+    }
+
+    public void handleResult(Result result) {
+        // Do something with the result
+        Log.w("handleResult", result.getText());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        // Resumes scanning
+        //ScannerView.resumeCameraPreview(this);
+    }
+}
