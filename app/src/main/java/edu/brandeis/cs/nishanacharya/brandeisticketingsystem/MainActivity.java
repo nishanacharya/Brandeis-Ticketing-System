ef4de6d251bae6fac1e5308f22ee35fe9565b7f8
Michael Jiang <Mjiang@Michaels-MacBook-Pro.local>
2017-12-03 00:38:59 -0500

    splash and toolbar

--- app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/MainActivity.java
+++ app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/MainActivity.java
@@ -2,6 +2,10 @@ package edu.brandeis.cs.nishanacharya.brandeisticketingsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu item) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.directory,item);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.home_Button:
+//                // return home
+//                return true;
+//
+//            default:
+//                // If we got here, the user's action was not recognized.
+//                // Invoke the superclass to handle it.
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//    }
 
     public String test(String test){ return test; }
 }
