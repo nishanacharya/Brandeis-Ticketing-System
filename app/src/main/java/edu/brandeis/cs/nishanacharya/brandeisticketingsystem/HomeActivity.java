b16a2dab3da55bd9ef9cdf2cfc0f03426163ae00
Nishan Acharya <nishan.acharya@gmail.com>
2017-12-02 20:17:29 -0500

    added signout function

--- app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/HomeActivity.java
+++ app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/HomeActivity.java
@@ -1,15 +1,19 @@
 package edu.brandeis.cs.nishanacharya.brandeisticketingsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView welcome;
+    private Button signOutButton;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -19,11 +23,22 @@ public class HomeActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
+        signOutButton = (Button) findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(this);
         welcome(user);
     }
 
     private void welcome(FirebaseUser user) {
         welcome = (TextView) findViewById(R.id.textWelcome);
         welcome.setText(user.getEmail());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == signOutButton){
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        }
     }
 }
