86b3e85f3832ea241582c2885678780dd5427fd1
Nishan Acharya <nishan.acharya@gmail.com>
2017-12-04 00:39:51 -0500

    partial work on admin home and creating event activity

---
+++ app/src/main/java/edu/brandeis/cs/nishanacharya/brandeisticketingsystem/CreateEventActivity.java
@@ -0,0 +1,18 @@
+package edu.brandeis.cs.nishanacharya.brandeisticketingsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+        createEvent();
+    }
+
+    private void createEvent() {
+
+    }
+}
